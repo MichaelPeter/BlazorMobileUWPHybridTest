b42fa4b0e0a1f6dfd6910978d95f1d6e824d8212
Michael Peter <michaelpeteronline@googlemail.com>
2021-01-05 19:23:29 +0100

    Add project files.

---
+++ BlazorMobileUWPHybridTest/BlazorMobileUWPHybridTest/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BlazorMobileUWPHybridTest.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BlazorMobileUWPHybridTest.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
